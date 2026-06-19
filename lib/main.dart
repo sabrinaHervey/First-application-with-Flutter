084b1b6c22032d65f5ffca3762909307142bd4dc
Sabrina Hervey <sabrina@centricular.com>
2026-06-19 14:34:31 +0200

    Modification nom du projet

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Quiz App',
+      title: 'Quiz Culture Générale',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
